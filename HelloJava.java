93007672056d63961a6d0ab257435edcf48af788
Saptarishi9831 <113447457+Saptarishi9831@users.noreply.github.com>
2023-06-02 10:40:36 +0530

    Add files via upload

---
+++ HelloJava.java
@@ -0,0 +1,26 @@
+
+public class HelloJava {
+	public void myMethod1() {
+		System.out.println(" myMethod1 ");
+	}
+	
+	public void myMethod2() {
+		System.out.println(" myMethod2 ");
+	}
+	
+	public void mymethod3() {
+		System.out.println(" mymethod3 ");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		HelloJava h = new HelloJava();
+		
+		h.myMethod2();
+
+	}
+
+}
+
+
